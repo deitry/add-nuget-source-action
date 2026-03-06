f846a47c1599dbedcf866b171f0ca78459cfa1be
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2026-03-06 23:43:46 +0100

    Move rollup to devDependencies, add tsib dependency

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,52 +1,54 @@
 // See: https://rollupjs.org/introduction/
 
-import commonjs from '@rollup/plugin-commonjs'
-import nodeResolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 
-const config = [{
-  input: 'src/main.ts',
+const config = [
+  {
+    input: 'src/main.ts',
 
-  output: {
-    esModule: true,
-    file: 'dist/main.js',
-    format: 'es',
-    sourcemap: false
+    output: {
+      esModule: true,
+      file: 'dist/main.js',
+      format: 'es',
+      sourcemap: false
+    },
+    plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
   },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
-},
-{
-  input: 'src/post.ts',
+  {
+    input: 'src/post.ts',
 
-  output: {
-    esModule: true,
-    file: 'dist/post.js',
-    format: 'es',
-    sourcemap: false
+    output: {
+      esModule: true,
+      file: 'dist/post.js',
+      format: 'es',
+      sourcemap: false
+    },
+    plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
   },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
-},
-{
-  input: 'src/common.ts',
+  {
+    input: 'src/common.ts',
 
-  output: {
-    esModule: true,
-    file: 'dist/common.js',
-    format: 'es',
-    sourcemap: false
+    output: {
+      esModule: true,
+      file: 'dist/common.js',
+      format: 'es',
+      sourcemap: false
+    },
+    plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
   },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
-},
-{
-  input: 'src/dotnet.ts',
+  {
+    input: 'src/dotnet.ts',
 
-  output: {
-    esModule: true,
-    file: 'dist/dotnet.js',
-    format: 'es',
-    sourcemap: false
-  },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
-}]
+    output: {
+      esModule: true,
+      file: 'dist/dotnet.js',
+      format: 'es',
+      sourcemap: false
+    },
+    plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
+  }
+];
 
-export default config
+export default config;
